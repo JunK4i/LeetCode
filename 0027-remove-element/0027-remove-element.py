c1eefa7aee3d8e58d2ab6d03ceb7ee76557ed28a
Koh Jun Kai <49807837+JunK4i@users.noreply.github.com>
2023-08-27 00:43:35 +0800

    Time: 38 ms (86.09%), Space: 16.4 MB (33.31%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,12 +1,17 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        n = len(nums)
-        match = []
-        for i in range (n):
-            if nums[i] == val:
-                match.append(nums[i])
-        for num in match:
-            nums.remove(num)
-            nums.append(num)
-        k = n-len(match)
-        return k
+        # n = len(nums)
+        # match = []
+        # for i in range (n):
+        #     if nums[i] == val:
+        #         match.append(nums[i])
+        # for num in match:
+        #     nums.remove(num)
+        #     nums.append(num)
+        # k = n-len(match)
+        index = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[index] = nums[i]
+                index +=1   
+        return index
