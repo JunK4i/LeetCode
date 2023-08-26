3f97f2978c79994a8557458353901c92044e859f
Koh Jun Kai <49807837+JunK4i@users.noreply.github.com>
2023-08-27 00:49:44 +0800

    Time: 38 ms (86.09%), Space: 16.4 MB (33.31%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,10 +1,20 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        n = len(nums)
-        m = 0
-        for i in range(n):
+        # n = len(nums)
+        # match = []
+        # for i in range (n):
+        #     if nums[i] == val:
+        #         match.append(nums[i])
+        # for num in match:
+        #     nums.remove(num)
+        #     nums.append(num)
+        # k = n-len(match)
+        
+        n = 0
+        l = len(nums)
+        for i in range(l):
             if nums[i] == val:
                 nums[i] = 51
-                m+=1
+                n += 1
         nums.sort()
-        return n-m
+        return l - n
