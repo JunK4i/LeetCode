ee805b85ac74544cc72aee174504cf8fe675ecc9
Koh Jun Kai <49807837+JunK4i@users.noreply.github.com>
2023-09-05 23:30:45 +0800

    Time: 49 ms (50.12%), Space: 16.7 MB (53.07%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -3,13 +3,10 @@ class Solution:
         citations.sort(reverse=True)
         cited_at_least = 1000
         published = 0
-        temp = []
+        result = 0
         for i in range(len(citations)):
             published = i+1
             if citations[i] < cited_at_least:
                 cited_at_least = citations[i]
-            temp.append(min(published, citations[i]))
-        #     print(f"{cited_at_least}, {published}")
-        # print(temp)
-
-        return max(temp)
+            result = max(result, min(published, citations[i]))
+        return result
