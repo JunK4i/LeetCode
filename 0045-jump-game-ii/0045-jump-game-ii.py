4022afadc66fc38fd619180bb480ee891ea66d07
Koh Jun Kai <49807837+JunK4i@users.noreply.github.com>
2023-09-05 19:19:45 +0800

    Time: 1376 ms (30.07%), Space: 17.4 MB (75.45%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,34 +1,34 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         # at each step find the farthest u can jump and the number of steps. 
-        # if you can jump further with less steps, update count
+        # if you can jump further with less steps, update indexes that can increase furthest jump
+        # When you have reached the end, start from the beginning and find the largest index that can increase
+        # max jumps
+
         n = len(nums)
         reachable = 0
-        jump_count = 0
         jumps = {}
         for i in range(n):
             if reachable >= n-1:
-                print(nums)
-                print(f"returning i:{i}, reachable:{reachable}, jump_count{jump_count}")
-                print(jumps)
+                # jumps store the indexes that extend "reachable"
+                # j keeps track of jumper index. 
+                # within each jump_distance, look for the largest index in jumps{} that can extend reachable
+                # repeat until jump_distance < reachable
                 j = 0
                 keys = list(jumps.keys())
                 jump_distance = 0
                 jump_count = 0
                 while jump_distance < reachable:
                     jump_distance = jumps[j]
+                    # filters indexes that are <= the previous jump, and finds the max
                     j = max(filter(lambda x: x <= jump_distance, keys))
                     jump_count+=1
-                    print(f"j:{j},jump_d:{jump_distance}")
-                    print(max(filter(lambda x: x <= jump_distance, keys)))
                 return jump_count
             if reachable < nums[i]+ i:
                 reachable = nums[i]+i
-                jump_count+=1
                 jumps[i] = reachable
             else:
                 continue
         
-        print(f"i:{i}, reachable:{reachable}, jump_count{jump_count}")
 
             
