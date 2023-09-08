7e37cde015b21fe5a4050e56c800e19a07acbc1d
Koh Jun Kai <49807837+JunK4i@users.noreply.github.com>
2023-09-08 15:26:48 +0800

    Time: 201 ms (53.86%), Space: 25.1 MB (26.33%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,22 +1,38 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        total = 1
-        zero = []
-        for i in range(len(nums)):
-            if(nums[i]==0):
-                zero.append(i)
-                continue
-            total*=nums[i]
+        
+        # this is a hack to deal with the division operation requirement by using exponential -1
+        # total = 1
+        # zero = []
+        # for i in range(len(nums)):
+        #     if(nums[i]==0):
+        #         zero.append(i)
+        #         continue
+        #     total*=nums[i]
+        # ans = []
+        # for i in range(len(nums)):
+        #     if (nums[i] != 0 and len(zero) != 0) or len(zero) > 1:
+        #         ans.append(0)
+        #     elif nums[i] == 0:
+        #         ans.append(total)
+        #     else:
+        #         ans.append(int(total*(nums[i]**-1)))
+        # return ans
 
-        print(total)
-        print(zero)
+        # Prefix, Suffix product, ans[i] = pre[i] * suff[i]. effectively removing i from product
+        n  = len(nums)
+        pre = [1] #contains product every element before i
+        suff = [1 for x in nums]
         ans = []
-        for i in range(len(nums)):
-            if (nums[i] != 0 and len(zero) != 0) or len(zero) > 1:
-                ans.append(0)
-            elif nums[i] == 0:
-                ans.append(total)
-            else:
-                ans.append(int(total*(nums[i]**-1)))
-        print(ans)        
+
+        for i in range(1, n):
+            pre.append(pre[i-1] * nums[i-1])
+        
+        for i in reversed(range (n-1)):
+            suff[i] = suff[i+1] * nums[i+1]
+        
+        for i in range(n):
+            ans.append(pre[i] * suff[i])
+
         return ans
+
