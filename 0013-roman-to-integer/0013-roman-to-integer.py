f277cd35375a73b3f14068d9e0ecf147a67efeee
Koh Jun Kai <49807837+JunK4i@users.noreply.github.com>
2023-09-15 22:18:27 +0800

    Time: 59 ms (31.07%), Space: 16.1 MB (99.83%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,32 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        total = 0 
+        dic = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        i=0
+        while i < len(s):
+            char = s[i]
+            if i < len(s)-1:
+                if char == "I" and (s[i+1] == "V" or s[i+1] == "X"):
+                    total += dic[s[i+1]] -1
+                    i+=2
+                elif char == "X" and (s[i+1] == "L" or s[i+1] == "C"):
+                    total += dic[s[i+1]] - 10
+                    i+=2
+                elif char == "C" and (s[i+1] == "D" or s[i+1] == "M"):
+                    total += dic[s[i+1]] - 100
+                    i+=2
+                else:
+                    total += dic[s[i]]
+                    i+=1
+            else:
+                total += dic[s[i]]
+                i+=1
+        return total
