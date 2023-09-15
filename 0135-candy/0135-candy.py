4290e06032a38bd45fd08863f4aebce0677525b0
Koh Jun Kai <49807837+JunK4i@users.noreply.github.com>
2023-09-15 13:41:07 +0800

    Time: 155 ms (29.37%), Space: 19.4 MB (28.41%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -5,15 +5,13 @@ class Solution:
         right = [1 for x in ratings]
 
         for i in range(1,n):
-            if ratings[i] > ratings [i-1]:
+            if ratings[i] > ratings[i-1]:
                 left[i] = left[i-1]+1
+
         for i in reversed(range(n-1)):
             if ratings[i] > ratings[i+1]:
                 right[i] = right[i+1]+1
-        print(left)
-        print(right)
-        for i in range(n):
-            left[i] = max(left[i],right[i])
+                left[i] = max(left[i],right[i])
         
         return sum(left)
         
